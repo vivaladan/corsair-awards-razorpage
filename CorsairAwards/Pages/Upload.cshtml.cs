55eaae7b1a020774f83115a20629b491ee27e6a9
Daniel Revell <daniel.revell@navico.com>
2020-06-21 00:46:30 +0100

    file upload

--- CorsairAwards/Pages/Upload.cshtml.cs
+++ CorsairAwards/Pages/Upload.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -20,6 +22,14 @@ namespace CorsairAwards.Pages
         public void OnGet()
         {
 
+        }
+        
+        [BindProperty]
+        public IFormFile Upload { get; set; }
+
+        public Task OnPostAsync()
+        {
+            return Task.CompletedTask;
         }
     }
 }
